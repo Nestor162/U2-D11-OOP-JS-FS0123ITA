f87d092518839fe5885c8751999fd59bb1a78214
Nestor162 <nestorcicardini@gmail.com>
2023-03-13 15:57:02 +0100

    Svolto esercizio 2

--- script.js
+++ script.js
@@ -18,3 +18,53 @@ const marioRossi = new User("Mario", "Rossi", 38, "Italy");
 const chiaraBianchi = new User("Chiara", "Bianchi", 22, "Italy");
 
 console.log(User.isOlder(marioRossi, chiaraBianchi));
+
+//Esercizio 2
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+
+  static sameOwner(pet1, pet2) {
+    return pet1.ownerName === pet2.ownerName
+      ? "The two pets have the same owner"
+      : "The two pets have different ownners";
+  }
+}
+
+// Array su cui salvare tutti le istanze Pet create tramite il form
+const petsArray = [];
+
+// viene selezionato il form
+let petForm = document.getElementById("addPetForm");
+
+petForm.addEventListener("submit", event => {
+  event.preventDefault(); //Evita che venga inviato il form
+
+  let FormOwnerName = document.getElementById("ownerName").value;
+  let FormPetName = document.getElementById("petName").value;
+  let FormSpecies = document.getElementById("species").value;
+  let FormBreed = document.getElementById("breed").value;
+
+  // Vengono ripuliti i campi imput
+  document.getElementById("ownerName").value = "";
+  document.getElementById("petName").value = "";
+  document.getElementById("species").value = "";
+  document.getElementById("breed").value = "";
+
+  // Viene creato una nuova istanza della classe pet
+  petsArray.push(new Pet(FormOwnerName, FormPetName, FormSpecies, FormBreed));
+
+  let list = document.querySelector("ol");
+  list.innerHTML = "";
+
+  for (pet of petsArray) {
+    console.log(pet);
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `<strong> Owner: </strong> ${pet.ownerName}, <strong> Pet:</strong> ${pet.petName}, <strong>Species:</strong> ${pet.species}, <strong>Breed:</strong> ${pet.breed}`;
+    list.appendChild(listItem);
+  }
+});
